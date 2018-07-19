0ca92e6938fe9710ccb6fb2a4dbcffe799575ff9
Robert Ng <anhngo.km@gmail.com>
2018-07-19 00:59:43 -0400

    Firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyALKe6fJRFmB9QGq2xbzrhqMVE-pJ3faNs",
+    authDomain: "la-cour-des-grands.firebaseapp.com",
+    databaseURL: "https://la-cour-des-grands.firebaseio.com",
+    projectId: "la-cour-des-grands",
+    storageBucket: "la-cour-des-grands.appspot.com",
+    messagingSenderId: "819997327109"
+  }
 };
