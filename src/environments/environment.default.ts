698b03c286d02883c6c97ea9b1ce1f4d8f329fb6
Robert Ng <anhngo.km@gmail.com>
2018-08-02 22:50:39 -0400

    Refactor code

---
+++ src/environments/environment.default.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyALKe6fJRFmB9QxxxxxhqMVE-xxxxxx",
+    authDomain: "xxxxxxx.firebaseapp.com",
+    databaseURL: "https://xxxxxxx.firebaseio.com",
+    projectId: "xxxxxxx",
+    storageBucket: "xxxxxxx.appspot.com",
+    messagingSenderId: "xxxxxxx"
+  }
+};
